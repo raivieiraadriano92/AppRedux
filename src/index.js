1527b1c88fd5388087458b749ef0945129e7fb6a
Raí Vieira Adriano <raivieiraadriano92@gmail.com>
2018-03-20 13:37:49 -0300

    start

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import 'config/ReactotronConfig';
+import store from 'store';
+import TodoList from './TodoList';
+
+const App = () => (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+);
+
+export default App;
---
+++ src/store/reducers/todos.js
@@ -0,0 +1,13 @@
+const initialState = [
+  { id: 0, text: 'Fazer café' },
+  { id: 1, text: 'Estudar GoNative' },
+];
+
+export default function todos(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, { id: Math.random(), text: action.payload.text }];
+    default:
+      return state;
+  }
+}
