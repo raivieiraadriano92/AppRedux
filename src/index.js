3f61959619462659883852d31ec778df503aa9ad
Raí Vieira Adriano <raivieiraadriano92@gmail.com>
2018-03-20 18:50:16 -0300

    novo app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
 import { Provider } from 'react-redux';
+import { View } from 'react-native';
+import Routes from 'routes';
 
 import 'config/ReactotronConfig';
 import store from 'store';
-import TodoList from './TodoList';
 
 const App = () => (
   <Provider store={store}>
-    <TodoList />
+    <Routes />
   </Provider>
 );
 
